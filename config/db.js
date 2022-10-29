ba11265183413c5dfdcc442f0902d9a5f97fef9a
nadeem <nadimbhati361@gmail.com>
2022-10-29 09:40:19 +0530

    db modified

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 const connectDB = async ()=>{
-    const bhati = await mongoose.connect("mongodb://localhost:27017");
+    
+    const bhati = await mongoose.connect("mongodb+srv://prod:aELt8iimgyhwPCbV@cluster0.fhesput.mongodb.net/?retryWrites=true&w=majority");
     console.log("bhati----",bhati.connection.host);
 };
 
